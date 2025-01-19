61f7d9d28646cc3da42d2a1e31eda48900740a6f
Esra <esraayaareb@gmail.com>
2025-01-19 12:02:26 +0300

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -26,20 +26,26 @@ function App() {
         </nav>
       </header>
       <main>
-        <section className="hero"></section>
+        <section className="hero">
+          <div className="hero_content">
+            <p className="hero_title_top">
+              <span className="badge">New</span>
+              <span>Monograph Dashboard</span>
+            </p>
+            <h1>Powerful insights into your team</h1>
+            <p className="hero_desc">
+              Project planning and time tracking for agile teams
+            </p>
+            <div className="hero_cta">
+              <a href="">Schedule a demo</a>
+              <span>to see a live preview</span>
+            </div>
+          </div>
+          <img src="assets/images/illustration-devices.svg" alt="Hero Image" />
+        </section>
       </main>
     </>
   );
 }
 
 export default App;
-// Login
-
-// New
-// Monograph Dashboard
-
-// Powerful insights into your team
-// Project planning and time tracking for agile teams
-
-// Schedule a demo
-// to see a live preview
